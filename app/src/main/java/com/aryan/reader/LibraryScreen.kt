ShiroiKuma0/shiroikuma-etsuran

Reader gestures: tap/swipe zones, real page turns, live overlay, page-turn sound
Tap-and-swipe (new "Tap and swipe" section on the 白い熊 UI page, per-gesture
toggles, master enable):
- A single mode-agnostic gesture layer on the reader's outer container intercepts
  in the Initial pass, so it works in paginated, native-vertical AND the default
  WebView vertical renderer alike. Side-third quick tap turns a page (long press
  still selects text); center tap/scroll and horizontal page-swipes pass through.
- Right-third vertical swipe steps font size, left-third steps brightness
  (WhiteBearGestureState, fork-local prefs).

Real page turns in the WebView renderer (window.whiteBearTurnPage):
- Instant full-viewport jump, no smooth-scroll; "Page turn amount" slider,
  100% (no overlap) by default.
- Crosses chapter boundaries: at a chapter end/start a tap flips to the next/prev
  chapter (its top/bottom) via the existing ChapterScrollPosition path.
- Snaps the top line whole (whiteBearSnapTopToLine) so a cut line becomes the
  first full line rather than starting mid-glyph.

Page-turn sound:
- Bundled page1..page5 ogg in res/raw, SoundPool on the Media channel
  (USAGE_MEDIA), default page1, Off/1-5 chooser with tap-to-preview; plays only
  on an actual turn.

Live swipe overlay: translucent yellow, black-outlined "Font N%" / "Brightness
N%" readout centered over the page while swiping, auto-fades.

Other fixes:
- Pure-black surfaces: surfaceTint = Transparent so Material3 tonal elevation no
  longer casts olive over the black top/bottom bars.
- The 白い熊 UI settings entry is now a rounded card under the search bar (new
  backward-compatible belowSearchContent slot on SharedSettingsHub).
- Long-press the Library top-bar Settings cog opens the UI page directly.
- BUILD_NUMBER bumped by the 1.0.51+11..+15 builds.

app/src/main/java/com/aryan/reader/LibraryScreen.kt
@@ -436,6 +436,7 @@ fun LibraryScreen(
             },
             onDeleteCatalogStreams = viewModel::deleteStreamedBooksForCatalog,
             onSettingsClick = { navController.navigate(AppDestinations.SETTINGS_SCREEN_ROUTE) },
+            onSettingsLongClick = { navController.navigate(AppDestinations.WHITE_BEAR_UI_SCREEN_ROUTE) },
             usePdfFileNameAsDisplayName = uiState.usePdfFileNameAsDisplayName
         )
 
@@ -754,6 +755,7 @@ fun LibraryScreenContent(
     onStreamOpdsBook: (OpdsEntry, OpdsCatalog?) -> Unit,
     onDeleteCatalogStreams: (String) -> Unit,
     onSettingsClick: () -> Unit,
+    onSettingsLongClick: () -> Unit = {},
     usePdfFileNameAsDisplayName: Boolean,
 ) {
     val isBookContextualModeActive = selectedItems.isNotEmpty()
@@ -969,7 +971,17 @@ fun LibraryScreenContent(
                                     Icon(Icons.Default.Search, contentDescription = stringResource(R.string.action_search))
                                 }
                             }
-                            IconButton(onClick = onSettingsClick) {
+                            // Long-press opens the 白い熊 書籍閲覧 UI page directly.
+                            Box(
+                                modifier = Modifier
+                                    .size(48.dp)
+                                    .clip(CircleShape)
+                                    .combinedClickable(
+                                        onClick = onSettingsClick,
+                                        onLongClick = onSettingsLongClick
+                                    ),
+                                contentAlignment = Alignment.Center
+                            ) {
                                 Icon(Icons.Default.Settings, contentDescription = stringResource(R.string.settings))
                             }
                         }

ShiroiKuma0/shiroikuma-shosekietsuran

app/src/main/java/com/aryan/reader/LibraryScreen.kt
@@ -486,6 +486,7 @@ fun LibraryScreen(
             onDeleteCatalogStreams = viewModel::deleteStreamedBooksForCatalog,
             onShowBanner = viewModel::showBanner,
             onSettingsClick = { navController.navigateIfReady(com.aryan.reader.shared.ui.SharedMobileAppDestination.SETTINGS) },
+            onSettingsLongClick = { navController.navigateIfReady(AppDestinations.WHITE_BEAR_UI_SCREEN_ROUTE) },
             usePdfFileNameAsDisplayName = uiState.usePdfFileNameAsDisplayName,
             cloudFolderSelection = cloudFolderSelection.takeIf { canUseCloudFolderSync },
             cloudSyncEnabled = uiState.isSyncEnabled,
@@ -807,6 +808,7 @@ fun LibraryScreenContent(
     onDeleteCatalogStreams: (String) -> Unit,
     onShowBanner: (String) -> Unit,
     onSettingsClick: () -> Unit,
+    onSettingsLongClick: () -> Unit = {},
     usePdfFileNameAsDisplayName: Boolean,
     cloudFolderSelection: CloudFolderSyncSelection? = null,
     cloudSyncEnabled: Boolean = false,
@@ -946,7 +948,19 @@ fun LibraryScreenContent(
                 )
                 IconButton(onClick = { onSearchActiveChange(true) }) { Icon(Icons.Default.Search, stringResource(R.string.action_search)) }
             }
-            IconButton(onClick = onSettingsClick) { Icon(Icons.Default.Settings, stringResource(R.string.settings)) }
+            // 白い熊: long-press the cog opens the UI page directly.
+            Box(
+                modifier = Modifier
+                    .size(48.dp)
+                    .clip(CircleShape)
+                    .combinedClickable(
+                        onClick = onSettingsClick,
+                        onLongClick = onSettingsLongClick
+                    ),
+                contentAlignment = Alignment.Center
+            ) {
+                Icon(Icons.Default.Settings, stringResource(R.string.settings))
+            }
         },
         filterChips = {
             if (pagerState.currentPage == 0) {